947a4fa6df4256055d76b4f07f512539700ea060
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2015-07-04 10:30:21 +0100

    added misc/

---
+++ src/main/java/org/xmlcml/cmine/misc/CMineUtil.java
@@ -0,0 +1,30 @@
+package org.xmlcml.cmine.misc;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.xmlcml.cmine.files.CMDir;
+
+public class CMineUtil {
+	
+	private static final Logger LOG = Logger.getLogger(CMineUtil.class);
+	static {
+		LOG.setLevel(Level.DEBUG);
+	}
+
+	/** Catch errors from. running ProcessBUilder with an uninstalled program
+	 *  
+	 * @param e
+	 * @param programName to run , e.g. 'latexml' , 'tesseract'
+	 */
+	public static void catchUninstalledProgram(IOException e, String programName) {
+		String error = e.getMessage();
+		if (error.startsWith("Cannot run program \""+programName+"\": error=2")) {
+			LOG.error("******** "+programName+" must be installed *************");
+		} else {
+			throw new RuntimeException("cannot convert file, ", e);
+		}
+	}
+
+}
