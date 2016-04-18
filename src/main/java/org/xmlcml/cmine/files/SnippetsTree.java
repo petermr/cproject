a0a7b762997488326600c5921893a920f1e99341
tarrow <tarrow@users.noreply.github.com>
2016-04-18 19:12:24 +0100

    refactor, test and annotate snippetstree (#8)
    Fixes reading windows separated file paths. fixes ami#44

--- src/main/java/org/xmlcml/cmine/files/SnippetsTree.java
+++ src/main/java/org/xmlcml/cmine/files/SnippetsTree.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.xmlcml.xml.XMLUtil;
@@ -84,13 +85,31 @@ public class SnippetsTree extends Element {
 		return this.toXML();
 	}
 
+	/**
+	 * Returns the value of the filename attribute of the first snippet in the snippetlist
+	 * The separators of this are converted to unix format if written on windows
+	 * will return null if the xml attribute called 'file' isn't set in the element
+	 * 
+	 * @return String | null
+	 */
 	public String getFilename() {
-		List<XMLSnippets> snippetsList = getOrCreateSnippetsList();
-		return snippetsList.size() == 0 ? null : snippetsList.get(0).getFilename();
+		List<XMLSnippets> snippetsList = getSnippetsList();
+		return snippetsList.size() == 0 ? null : FilenameUtils.separatorsToUnix(snippetsList.get(0).getFilename());
 	}
 
-	public List<XMLSnippets> getOrCreateSnippetsList() {
+	public List<XMLSnippets> getSnippetsList() {
 		if (snippetsList == null) {
+			createSnippetsList();
+		}
+		return snippetsList;
+	}
+	
+	@Deprecated
+	public List<XMLSnippets> getOrCreateSnippetsList() {
+		return getSnippetsList();
+	}
+	
+	private void createSnippetsList() {
 			getOrCreateSnippetsChildren();
 			snippetsList = new ArrayList<XMLSnippets>();
 			for (Element element : snippetsElementList) {
@@ -98,15 +117,8 @@ public class SnippetsTree extends Element {
 				LOG.trace("snippets:"+snippets);
 				snippetsList.add(snippets);
 			}
-		}
-		return snippetsList;
 	}
 
-	/**
-	 * /foo/bar/projectName/results/plugin/option/results.xml
-	 
-	 * @return
-	 */
 	public String getCTreeName() {
 		if (cTreeName == null) {
 			getFilenameComponents();
@@ -135,6 +147,10 @@ public class SnippetsTree extends Element {
 		return optionName;
 	}
 
+	/**
+	 * Sets attributes of the snippetsTree by parsing (using a regex)
+	 * the 'file' attribute of the 1st XML snippet
+	 */
 	private void getFilenameComponents() {
 		String filename = getFilename();
 		if (filename != null) {
